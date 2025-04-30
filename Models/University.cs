c8664f05f85ee93fcc85ff194ea27fcb39c9d422
Thelry D. Bissi <thelry.d.b@hotmail.com>
2025-04-30 15:33:28 -0300

    Fix nullable props

--- Models/University.cs
+++ Models/University.cs
@@ -2,9 +2,9 @@ namespace backend.Models
 {
     public class University
     {
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public List<string> WebPages { get; set; }
-        public List<string> Domains { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public List<string>? WebPages { get; set; }
+        public List<string>? Domains { get; set; }
     }
 }
